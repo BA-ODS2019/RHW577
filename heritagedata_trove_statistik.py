c1fb6b2d5297242db3092ef1a737f75bde550153
tkay8 <torkellingvars@gmail.com>
2019-11-18 16:21:29 +0100

    Update heritagedata_trove_statistik.py

--- heritagedata_trove_statistik.py
+++ heritagedata_trove_statistik.py
@@ -6,3 +6,32 @@ Created on Mon Nov 18 16:14:29 2019
 @author: torkellingvarsson
 """
 
+import requests
+import pandas as pd
+import numpy as np
+from pandas.io.json import json_normalize
+
+# variable med min API-key til Trove
+api_key = '6majqpp3si922qkj'
+print('Your API key is: {}'.format(api_key))
+
+api_search_url = 'https://api.trove.nla.gov.au/v2/result'
+
+# søge parametre (dette svare til den URL som man laver i browser vinduet på Trove)
+params = {
+        'q': 'mahatma+gandhi',
+        'zone': 'newspaper',
+        'key': api_key,
+        'n': '100',
+        'encoding': 'json',
+        'facet': 'year',
+        'n': 0
+        }
+response = requests.get(api_search_url, params=params)
+print (response)
+
+json = response.json()
+
+# laver nu en dataframe via pandas
+df = json_normalize(json['response']['zone'][0]['facets']['facet']['term'])
+df.head()
